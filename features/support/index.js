a53ea5d18e5a723546d09b1bec519205b1758348
Danilo Policarpo <danilopolicarpo14@gmail.com>
2019-08-27 17:46:56 -0300

    add cucumber report html

---
+++ features/support/index.js
@@ -0,0 +1,17 @@
+var reporter = require('cucumber-html-reporter');
+ 
+var options = {
+        theme: 'bootstrap',
+        jsonFile: 'report/cucumber_report.json',
+        output: 'report/cucumber_report.html',
+        reportSuiteAsScenarios: true,
+        launchReport: true,
+        metadata: {
+            "App Version":"0.1",
+            "Test Environment": "PRODUÇÃO",
+            "Platform": "ANDROID",
+            "Executed": "LOCAL"
+        }
+    };
+ 
+    reporter.generate(options);
--- readme.md
+++ readme.md
@@ -3,4 +3,10 @@ https://github.com/admc/wd
 https://github.com/appium-boneyard/sample-code/tree/master/sample-code/examples/node
 
 https://github.com/cucumber/cucumber-js
+
+
+#REPORT 
+https://www.npmjs.com/package/cucumber-html-reporter
 # trabalhando ainda !!!!!!
+
+
